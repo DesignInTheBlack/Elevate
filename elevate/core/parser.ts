5a21847ea00213374fee43c87a186d47832912a5
K. Pickett <designintheblack@gmail.com>
2025-01-17 23:05:21 -0500

    Update direct property definition to differentiate further from property for use in contextual flags.

--- elevate/core/parser.ts
+++ elevate/core/parser.ts
@@ -13,7 +13,10 @@ import type { CstNode } from "chevrotain";
 // ╚════════════════════════════════════════════════════════════════════╝
 const State = createToken({ name: "stateFlag", pattern: /@[a-zA-Z0-9-]+:/ });
 const openState = createToken({ name: "openState", pattern: /\[/ });
-const DirectProperty = createToken({ name: "DirectProperty", pattern: /[-a-zA-Z][a-zA-Z0-9_-]*/ });
+const DirectProperty = createToken({ 
+    name: "DirectProperty", 
+    pattern: /[-a-zA-Z][a-zA-Z0-9_-]*(?!:)/ 
+});
 const PassProperty = createToken({ 
     name: "PassProperty", 
     pattern: /:([\w\-()'".,\s:\/\.@#%=&?]+)/ 
@@ -64,15 +67,28 @@ class ElevateParser extends CstParser {
         $.RULE("ContextBlock", () => {
             $.CONSUME(State);
             $.CONSUME(openState);
+        
             $.MANY(() => {
-                $.CONSUME(Property);
-                $.MANY2(() => {
-                    $.CONSUME(Modifier);
-                });
+                $.OR([
+                    {
+                        ALT: () => {
+                            $.CONSUME(DirectProperty); // Matches a direct property (no modifiers)
+                        },
+                    },
+                    {
+                        ALT: () => {
+                            $.CONSUME(Property); // Matches a property
+                            $.MANY2(() => {
+                                $.CONSUME(Modifier); // Matches modifiers for the property
+                            });
+                        },
+                    },
+                ]);
             });
+        
             $.CONSUME(closeState);
         });
-
+        
 
         $.RULE("PassthroughBlock", () => {
             $.CONSUME(Property);
