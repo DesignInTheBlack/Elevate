4befbe0d6250fe6ae792cbd432a172af05621964
K. Pickett <designintheblack@gmail.com>
2025-09-22 15:58:11 -0400

    Update Standard Regex Templates for Config

--- elevate/config/elevate.ts
+++ elevate/config/elevate.ts
@@ -4,11 +4,13 @@ const options = {
     Output:'./', //Where to put the compiled CSS
     Extend:[], //CSS files to include as well
     ClassRegex: [
-        /\bclass\s*=\s*"([^"]*)"/g,                // class="..."
-        /\bclass\s*=\s*'([^']*)'/g,                // class='...'
-        /\bclassName\s*=\s*"([^"]*)"/g,            // className="..."
-        /\bclassName\s*=\s*'([^']*)'/g,            // className='...'
-        /\bclassName\s*=\s*{\s*`([\s\S]*?)`\s*}/g  // className={`...`}
+      /\bclass\s*=\s*"([^"]*)"/g,                // class="..."
+      /\bclass\s*=\s*'([^']*)'/g,                // class='...'
+      /\bclassName\s*=\s*"([^"]*)"/g,            // className="..."
+      /\bclassName\s*=\s*'([^']*)'/g,            // className='...'
+      /\bclassName\s*=\s*{\s*`([\s\S]*?)`\s*}/g,  // className={`...`}
+      /\bclassName\s*=\s*{\s*"([^"]*)"\s*}/g,   // className={"..."}
+      /\bclassName\s*=\s*{\s*'([^']*)'\s*}/g,   // className={'...'}
       ],
     SafeList:[]
 }
